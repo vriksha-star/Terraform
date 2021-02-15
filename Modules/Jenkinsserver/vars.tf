b75b5d976b6503a9b3f0a5799002ee33ffdd2215
vriksha-star <79082664+vriksha-star@users.noreply.github.com>
2021-02-15 15:19:32 +0530

    Update vars.tf

--- Modules/Jenkinsserver/vars.tf
+++ Modules/Jenkinsserver/vars.tf
@@ -2,5 +2,5 @@ variable "keyname"{}
 variable "vpcid"{}
 variable "ami_id"{}
 variable "iam_policy_arn"{
-	default = ["arn:aws:iam::aws:policy/AdministratorAccess"]
+	default = [“arn:aws:iam::aws:policy/AmazonEC2FullAccess”,“arn:aws:iam::aws:policy/AmazonRDSFullAccess”]
 }

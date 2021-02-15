ba6b2e0769b74dcbe4b11bf745f3b32f740d864f
vriksha-star <79082664+vriksha-star@users.noreply.github.com>
2021-02-15 15:20:04 +0530

    Update var.tf

--- Modules/TargetServer/var.tf
+++ Modules/TargetServer/var.tf
@@ -3,5 +3,5 @@ variable "vpcid"{}
 variable "tar_ami_id"{}
 variable "instance_count" {}
 variable "iam_policy_arn"{
-	default = ["arn:aws:iam::aws:policy/AdministratorAccess"]
+	default = [“arn:aws:iam::aws:policy/AmazonEC2FullAccess”,“arn:aws:iam::aws:policy/AmazonRDSFullAccess”]
 }
